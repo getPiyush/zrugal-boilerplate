717d8b507de8ceb77c9103e23f3c0ad24a13f75d
Piyush Praharaj <piyush.praharaj@outlook.com>
2021-09-07 00:57:59 +0530

    chore : utility changes

--- src/components/chitra/imageUtility.js
+++ src/components/chitra/imageUtility.js
@@ -1,3 +1,5 @@
+import Canvas from "./Canvas";
+
 export const getCanvasImageFromFile = (file) => {
   let imagedata = imageLoader(loadImage(file));
   return imagedata;
@@ -22,4 +24,106 @@ const loadImage = (file) => {
       fr.readAsDataURL(file);
     } else reject("FileReader not supported!");
   });
+};
+
+/// canvas functions
+export const loadImageToCanvas = (imageData) => {
+  const draw = (context, canvas) => {
+    var image = new Image();
+    image.src = imageData;
+    image.onload = () => {
+      console.log(image.width + "x" + image.height);
+      canvas.width = image.width;
+      canvas.height = image.height;
+      context.drawImage(image, 0, 0, image.width, image.height);
+    };
+  };
+  return <Canvas draw={draw} />;
+};
+
+export const convertToEffect = (effect) => {
+  const draw = (context, canvas) => {
+    console.log(
+      "context.width, context.height, canvas.width, canvas.height",
+      context.width,
+      context.height,
+      canvas.width,
+      canvas.height
+    );
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+    let newData = null;
+    switch (effect) {
+      case "grayscale":
+        newData = toGrayscale(imageData);
+        break;
+
+      case "dotted":
+        newData = toPixels(imageData);
+        break;
+
+      case "invert":
+        newData = invert(imageData);
+        break;
+      case "special":
+        newData = special(imageData);
+        break;
+
+      default:
+        newData = imageData;
+    }
+    context.putImageData(newData, 0, 0);
+  };
+  return <Canvas draw={draw} />;
+};
+
+var toGrayscale = function (imageData) {
+  const data = imageData.data;
+  console.log("data.length = ", data.length);
+  for (var i = 0; i < data.length; i += 4) {
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    data[i] = avg; // red
+    data[i + 1] = avg; // green
+    data[i + 2] = avg; // blue
+  }
+  return imageData;
+};
+
+var special = function (imageData) {
+  const data = imageData.data;
+  console.log("data.length = ", data.length);
+  for (var i = 0; i < data.length; i += 4) {
+    const avg = Math.max.apply(Math, [data[i], data[i + 1], data[i + 2]]);
+    data[i] = avg; // red
+    data[i + 1] = avg; // green
+    data[i + 2] = avg; // blue
+  }
+  return imageData;
+};
+
+var toPixels = function (imageData) {
+  const data = imageData.data;
+  console.log("data.length = ", data.length);
+  for (var i = 0; i < data.length; i += 4) {
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    data[i] = filterHigherPixel(data[i], avg); // red
+    data[i + 1] = filterHigherPixel(data[i + 1], avg); // green
+    data[i + 2] = filterHigherPixel(data[i + 2], avg); // blue
+  }
+  return imageData;
+};
+
+var invert = function (imageData) {
+  const data = imageData.data;
+  console.log("data.length = ", data.length);
+  for (var i = 0; i < data.length; i += 4) {
+    data[i] = 255 - data[i]; // red
+    data[i + 1] = 255 - data[i + 1]; // green
+    data[i + 2] = 255 - data[i + 2]; // blue
+  }
+  return imageData;
+};
+
+var filterHigherPixel = (colorInt, avg) => {
+  return colorInt > 100 ? 100 : avg;
 };
--- src/components/chitra/index.js
+++ src/components/chitra/index.js
@@ -1,39 +1,47 @@
-import React, { useState } from "react";
-import Canvas from "./Canvas";
-import { getCanvasImageFromFile } from "./imageUtility";
+import React, { useState, useEffect } from "react";
+import {
+  getCanvasImageFromFile,
+  loadImageToCanvas,
+  convertToEffect
+} from "./imageUtility";
 
 export default function Chitra() {
-  const [appState, setAppState] = useState();
-  const [imageFile, setImageFile] = useState();
+  const [appState, setAppState] = useState("init");
+  const [imageFile, setImageFile] = useState(null);
+  const [canvas, setCanvas] = useState(null);
+
+  useEffect(() => {
+    if (imageFile !== null) setCanvas(loadImageToCanvas(imageFile));
+  }, [imageFile]);
+
+  useEffect(() => {
+    if (canvas !== null) setAppState("imageSelected");
+  }, [canvas]);
 
   const userSelectedImage = (evt) => {
     getCanvasImageFromFile(evt.target.files[0]).then((data) => {
       console.log("got then response ..");
       setImageFile(data);
-      setAppState("imageSelected");
     });
   };
-
-  const loadImageToCanvas = (imageData) => {
-    const draw = (context, canvas) => {
-      var image = new Image();
-      image.src = imageData;
-      image.onload = () => {
-        console.log(image.width + "x" + image.height);
-        canvas.width = image.width;
-        canvas.height = image.height;
-        context.drawImage(image, 0, 0, image.width, image.height);
-      };
-    };
-    return <Canvas draw={draw} />;
+  const buttonClicked = (evt) => {
+    console.log(evt.target.value);
+    setAppState(evt.target.value);
   };
   const renderEffectsButtons = () => {
     return (
       <div>
-        <span>Apply effects &gt; </span>
-        <button>grayscale</button>
-        <button>dotted</button>
-        <button>spacial enhance</button>
+        <select name="effects" onChange={buttonClicked} id="canvasEffect">
+          <option value="reset">--Please choose an effect--</option>
+          <option value="grayscale">grayscale</option>
+          <option value="dotted">dotted</option>
+          <option value="invert">invert</option>
+          <option value="special">spacial enhance</option>
+        </select>
+        &nbsp;
+        <button value="reset" onClick={buttonClicked}>
+          reset
+        </button>
       </div>
     );
   };
@@ -54,7 +62,27 @@ export default function Chitra() {
           <div>
             {renderEffectsButtons()}
             <br />
-            {loadImageToCanvas(imageFile)}
+            {canvas}
+          </div>
+        );
+      case "grayscale":
+      case "dotted":
+      case "special":
+      case "invert":
+        return (
+          <div>
+            {renderEffectsButtons()}
+            <br />
+            {convertToEffect(appState)}
+          </div>
+        );
+
+      case "reset":
+        return (
+          <div>
+            {renderEffectsButtons()}
+            <br />
+            {canvas}
           </div>
         );
 
